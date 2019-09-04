48e806e223289048112d7ca17e7ca8af133c4473
Eduardo Campacci <campacc@gmail.com>
2019-09-04 17:26:56 -0300

    Primeiro commit booleanos

--- Fundamentos/booleanos.py
+++ Fundamentos/booleanos.py
@@ -3,12 +3,17 @@
 # Declaracao de variaveis
 n1 = 3
 n2 = 4
+nota = n1 + n2
 
 # Imprimindo resultado na tela, convertendo o inteiro p/ string.
 # Pois não é possivel concatenar inteiro com string.
-print('Nota final: ' + str(n1+n2))
+print('Nota final: ' + str(nota))
 
-# Booleano
-if str(n1+n2) >= 5:
-    True
-dad
+# Booleano True e False
+if nota >= 5:
+    nota = True
+    print(nota)
+
+else:
+    nota = False
+    print(nota)
