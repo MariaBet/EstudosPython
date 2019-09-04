66738d323554493706cf3ffcedb1fc41972000be
Eduardo Campacci <campacc@gmail.com>
2019-09-04 15:32:32 -0300

    praticando booleanos

---
+++ Fundamentos/booleanos.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Declaracao de variaveis
+n1 = 3
+n2 = 4
+
+# Imprimindo resultado na tela, convertendo o inteiro p/ string.
+# Pois não é possivel concatenar inteiro com string.
+print('Nota final: ' + str(n1+n2))
+
+# Booleano
+if str(n1+n2) >= 5:
+    True
--- Fundamentos/chacal2.py
+++
@@ -1,2 +0,0 @@
-print ("salve \o/")
-print("Hello world da Bet")
