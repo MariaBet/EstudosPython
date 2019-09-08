063db54245e5baf42882c644ff513a8fa61b6cd7
mariaBet <mariabetania543@gmail.com>
2019-09-08 09:06:28 -0300

    Exemplo de boleano com True e False

---
+++ Fundamentos/exemploBolean2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# exemplo True
+n1 = 28
+n2 = 34
+
+comparacao = n1 < n2
+print comparacao
+
+n1 = 28
+n2 = 34
+
+comparacao = n1 != n2
+print comparacao
+
+# exemplo False
+n1 = 28
+n2 = 34
+
+comparacao = n1 == n2
+print comparacao
+
+n1 = 28
+n2 = 34
+
+comparacao = n1 > n2
+print comparacao
