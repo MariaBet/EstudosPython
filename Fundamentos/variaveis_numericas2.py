ba0cbf56c7ec6d551cf2b395004d08b959fc5098
Eduardo Campacci <campacc@gmail.com>
2019-09-09 11:34:39 -0300

    identificando o tipo de variavel

---
+++ Fundamentos/variaveis_numericas2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Receber um valor qualquer e identificar o tipo da variavel
+# de acordo com o valor fornecido
+# Para valor string digite com aspas simples para o python
+# identificar que e uma string
+valor = input('Digite um valor: ')
+print(type(valor))
