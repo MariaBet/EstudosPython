6463eedf5994314c5dc69243ad0decd966becffa
Eduardo Campacci <campacc@gmail.com>
2019-09-10 14:54:43 -0300

    Incluido while em lista de carros.

--- Fundamentos/lista3.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-carros = []
-nome = input("Digite o modelo do carro: ")
-print("nome")
--- Fundamentos/lista3_carros.py
+++ Fundamentos/lista3_carros.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Lista
 carros = []
-
-# Input de elementos
 nome = input("Digite o modelo do carro: ")
 
+# Enquanto o nome do carro for diferente de zero
+# ele vai continuar incluindo elementos
+# até que a string 0 seja digitada
 while nome != '0':
     carros.append(nome)
     carros.sort()
