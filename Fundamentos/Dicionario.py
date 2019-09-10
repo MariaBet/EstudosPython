edf9b0c621f5f22c05db5422efdfabc93cf1d01d
mariaBet <mariabetania543@gmail.com>
2019-09-10 15:59:20 -0300

    Dicionário

---
+++ Fundamentos/Dicionario.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# Dicionário
+# Um dicionário é  mutável e indexada.
+# No Python, os dicionários são escritos com colchetes e possuem valores.
+
+Floresta = {
+    "Arvore": "Ipe",
+    "Felino": "onca",
+    "Madeira": "Sucupira",
+    "Aves" : "Sabia",
+    "Lepidopera": "Borboleta"
+}
+print(Floresta)
