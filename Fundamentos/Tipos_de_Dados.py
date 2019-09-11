5354b7e7d9ab78df023a9a611106ff3becbbc491
mariaBet <mariabetania543@gmail.com>
2019-09-11 14:29:19 -0300

    Tipo de dados comentarios

---
+++ Fundamentos/Tipos_de_Dados.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+texto extraido do w3schools <https://www.w3schools.com/python/python_datatypes.asp>
+O Python possui os seguintes tipos de dados embutidos por padrão, nestas categorias:
+Tipo de texto:	str
+Tipos numéricos:	int, float, complex
+Tipos de sequência:	list, tuple, range
+Tipo de mapeamento:	dict
+Definir tipos:	set, frozenset
+Tipo booleano:	bool
+Tipos binários:	bytes, bytearray, memoryview
+para obter o tipo de dado é usando o type()
+x = "Hello World"	str
+x = 20        	int
+x = 20.5	float
+x = 1j	complex
+x = ["apple", "banana", "cherry"]	list
+x = ("apple", "banana", "cherry")	tuple
+x = range(6)	range
+x = {"name" : "John", "age" : 36}	dict
+x = {"apple", "banana", "cherry"}	set
+x = frozenset({"apple", "banana", "cherry"})	frozenset
+x = True	bool
+x = b"Hello"	bytes
+x = bytearray(5)	bytearray
+x = memoryview(bytes(5))	memoryview
+"""
+
+"""
+Números Python
+Existem três tipos numéricos no Python:
+
+int
+flutuador
+complexo
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+Int
+Int, ou inteiro, é um número inteiro, positivo ou negativo, sem decimais,
+de comprimento ilimitado.
+
+Float
+Flutuar ou "número de ponto flutuante" é um número, positivo ou negativo,
+contendo uma ou mais casas decimais.
+
+Complexo
+Números complexos são escritos com um "j" como a parte imaginária:
+"""
