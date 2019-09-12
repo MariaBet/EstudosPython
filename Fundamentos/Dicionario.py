6cd0cd14cd56f78aa1995eb4349ed009f565aa3d
Eduardo Campacci <campacc@gmail.com>
2019-09-12 15:15:15 -0300

    Incluindo chaves e valores em um dicionario, verificando a quantidade  e excluindo elementos.

--- Fundamentos/Dicionario.py
+++ Fundamentos/Dicionario.py
@@ -20,3 +20,30 @@ Floresta = {
 Floresta["Pequi"] = "Brasilense"
 for x in Floresta.values():
     print(x)       # imprimi cada nome da lista que corresponde ao x
+
+
+chave = input("Digite uma categoria: ")
+valor = input("Digite uma especie: ")
+
+while chave != '0' or valor != '0':
+    # Adicionando valores no dicionario
+    # O metodo append() que é usado em lista não funciona em dicionarios
+    # A sintaxe para adicionar um valor em dicionario é diferente, veja
+    # abaixo ... dicionario[chave] = valor
+    Floresta[chave] = valor
+
+    # Imprimindo dicionario contatos
+    print(Floresta)
+
+    chave = input("Digite uma categoria: ")
+    valor = input("Digite uma especie: ")
+
+print(Floresta)
+
+# Quantidade de itens no dicionario Floresta
+len(Floresta)
+
+# Deletando um item do dicionario com del()
+excluir = input("Digite o nome de um elemento para excluir: ")
+del(Floresta[excluir])
+print(Floresta)
