cc3baa7242d7d1c60357f9924863b4c950a6f61d
Eduardo Campacci <campacc@gmail.com>
2019-09-12 10:28:52 -0300

    Incrementado trecho em Floreste e criado novo dicionario contatos.

--- Fundamentos/Dicionario.py
+++ Fundamentos/Dicionario.py
@@ -20,3 +20,18 @@ Floresta = {
 Floresta ["Pequi"] = "Brasilense"
 for x in Floresta.values():  # imprimi cada nome da lista
     print(x)
+
+
+
+# Fazendo input no dicionario, indicando chave e valor.
+chave = input("Digite uma chave para dicionario: ")
+valor = input("Digite um valor para a chave do dicionaio: ")
+
+# Adicionando valores no dicionario
+# O metodo append() que é usado em lista não funciona em dicionarios
+# A sintaxe para adicionar um valor em dicionario é diferente, veja
+# abaixo ... dicionario[chave] = valor
+Floresta[chave] = valor
+
+# Imprimindo dicionario contatos
+print(Floresta)
