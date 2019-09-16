420afc1090716a5ca25b8ea878ab966afcbcc75e
Eduardo Campacci <campacc@gmail.com>
2019-09-16 16:44:15 -0300

    Exemplo loop for.

---
+++ Fundamentos/loop_for.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Listas
+carros = ['Fox','Ferrari','Fusca','Mercedes']
+numeros = [20,50,90,200,110]
+
+# Realizando a soma da lista numeros
+soma = 0
+for item in numeros:
+    soma += item
+    print('Valor da soma é.: ',soma)
+
+# Imprimindo a lista em um loop
+for item in carros:
+    print(item)
