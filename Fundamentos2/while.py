129c0aff495c08eea76e1f19fdfc985d0cd05dc4
mariaBet <mariabetania543@gmail.com>
2019-09-17 16:09:10 -0300

    update

--- Fundamentos2/while.py
+++ Fundamentos2/while.py
@@ -10,11 +10,11 @@ tentativa = 5
 while tentativa > 0:
     print("Tentativa: ", tentativa)
     numero_da_sorte = int(input("Digite um numero da sorte: "))
+    tentativa -= 1
     if numero_da_sorte == 33:
         print("Voce acertou, seu premio eh de R$1000,00 " '\n')
         break
     else:
         print("Voce errou nao recebeu nenhum valor" '\n')
-    tentativa -= 1
 
 print("Fim da sorte!")
