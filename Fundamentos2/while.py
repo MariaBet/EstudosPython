b1e93a98c1b21b849cbc7e703c1d011836895523
mariaBet <mariabetania543@gmail.com>
2019-09-17 15:08:27 -0300

    update

---
+++ Fundamentos2/while.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+loop for usando o range que define quantidade valores
+"""
+numero_da_sorte = 33
+tentativas = 5
+
+while tentativas <= 5:
+    print("Tentativa: ", tentativas)
+    numero_da_sorte = int(input("Digite um numero da sorte: "))
+    if numero_da_sorte == 33:
+        print("Voce acertou, seu premio eh de R$1000,00 " '\n')
+        break
+    elif numero_da_sorte != 33:
+        print("Voce errou nao recebeu nenhum valor" '\n')
+
+print("Fim da sorte!")
+
+
+# parar o loop caso acerte o numero
