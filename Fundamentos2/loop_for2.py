86160b224c0a29bf73ddee5afa57f311dde93018
mariaBet <mariabetania543@gmail.com>
2019-09-17 11:13:23 -0300

    Add loop_for2

---
+++ Fundamentos2/loop_for2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+loop for usando o range que define quantidade valores
+"""
+numero_da_sorte = 33
+tentativas = 5
+
+for tentativas in range(5):
+    print("Tentativa: ", tentativas)
+    numero_da_sorte = int(input("Digite um numero da sorte: "))
+    if numero_da_sorte == 33:
+        print("Voce acertou, seu premio eh de R$1000,00 " '\n')
+    elif numero_da_sorte != 33:
+        print("Voce errou nao recebeu nenhum valor" '\n')
+
+print("Fim da sorte!")
+
+
+# parar o loop caso acerte o numero
