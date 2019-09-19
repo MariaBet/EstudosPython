32bf71ad9182d7149095596ed498fa938953afa4
Eduardo Campacci <campacc@gmail.com>
2019-09-19 09:14:08 -0300

    exemplo loop for com continue.

---
+++ Fundamentos2/loop_for_continue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    print('Achei \o/')
+    continue
+    print('Vamos continuar o fluxo')
+  print(x)
