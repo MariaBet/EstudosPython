b96c8f9c8bedb96ee74480de48f215a1c69a4088
mariaBet <mariabetania543@gmail.com>
2019-09-19 15:23:33 -0300

    funcao

---
+++ Fundamentos2/funcao.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+def cadastro():
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    print(nome, "Minha idade eh: ", idade, '\nMaravilha!!!\n')
+
+
+cadastro()
+cadastro()
+cadastro()
