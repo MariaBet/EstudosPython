e919ca14aa6b833dc7565269daaf9c9c208543d7
Eduardo Campacci <campacc@gmail.com>
2019-09-24 14:12:04 -0300

    update

--- Fundamentos2/funcao_calc.py
+++ Fundamentos2/funcao_calc.py
@@ -17,3 +17,16 @@ def divisao (n1, n2):
 
 def resto (n1, n2):
     print('O resto da divisão é.: ',n1%n2)
+
+
+""" Inputs """
+n1 = int(input('Digite o primeiro valor.: '))
+n2 = int(input('Digite o segundo valor.: '))
+print('\n')
+
+""" Chamando as funções """
+soma(n1, n2)
+subtracao(n1, n2)
+multiplicacao(n1, n2)
+divisao(n1, n2)
+resto(n1, n2)
