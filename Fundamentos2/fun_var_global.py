ff48fa5e6a319bb3ba89b7eea8d32b26f124a45a
Eduardo Campacci <campacc@gmail.com>
2019-09-24 10:36:16 -0300

    atualização

--- Fundamentos2/fun_var_global.py
+++ Fundamentos2/fun_var_global.py
@@ -11,14 +11,17 @@ num = 10
 def my_fun():
     global num #Aqui definimos que a variavel é global
     num = 20 #Esta variavel global recebe um valor
-    print(num) #Aqui é impresso o valor da variavel global
+    #print(num) #Aqui é impresso o valor da variavel global
+
 
 # Este print é da variavel comum, a função ainda não foi chamada. Portanto
 # a global ainda não existe.
-print(num)
+print("O valor comum de NUM é.: ", num, '\n')
 
 # Chamada a função, a partir de agora a variavel global existe.
 my_fun()
+print("O valor da variavel apenas dentro da função NUM é.: ", num, '\n' )
+
 
 # Impressão da variavel
-print(num)
+print("O valor de NUM agora sendo global e sobrepondo é.: ", num, '\n')
