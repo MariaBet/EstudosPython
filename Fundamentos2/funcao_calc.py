bf7d0c6ddef23b480843bcb81638e83c2cdffd76
Eduardo Campacci <campacc@gmail.com>
2019-09-24 13:49:36 -0300

    commit função calc.

---
+++ Fundamentos2/funcao_calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+def soma (n1, n2):
+    print('A soma é.: ',n1+n2)
+
+def subtracao (n1, n2):
+    print('A subtração é.: ',n1-n2)
+
+def multiplicacao (n1, n2):
+    print('A multiplicação é.: ',n1*n2)
+
+def divisao (n1, n2):
+    print('A divisao é.: ',n1/n2)
+
+def resto (n1, n2):
+    print('O resto da divisão é.: ',n1%n2)
