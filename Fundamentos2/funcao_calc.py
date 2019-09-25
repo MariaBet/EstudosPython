ec7c29758eaad9b2ba813bc9a04b965b9745b7e8
mariaBet <mariabetania543@gmail.com>
2019-09-25 14:31:49 -0300

    upadate indentação

--- Fundamentos2/funcao_calc.py
+++ Fundamentos2/funcao_calc.py
@@ -2,25 +2,32 @@
 # -*- coding: utf-8 -*-
 
 """ Funções """
+
+
 def soma(n1, n2):
     print('A soma é.: ', n1 + n2)
+
 
 def subtracao(n1, n2):
     print('A subtração é.: ', n1 - n2)
 
+
 def multiplicacao(n1, n2):
     print('A multiplicação é.: ', n1 * n2)
+
 
 def divisao(n1, n2):
     print('A divisao é.: ', n1 / n2)
 
+
 def resto(n1, n2):
     print('O resto da divisão é.: ', n1 % n2)
+
 
 """ Menu """
 opera = 0
 while opera != 'e':
-    opera = input( '''Escolha a operação:
+    opera = input('''Escolha a operação:
     + p/ Soma
     - p/ Subtracao
     / p/ Divisão
@@ -34,28 +41,28 @@ while opera != 'e':
     n2 = int(input('Digite o segundo valor.: '))
 
     """ Chamando as funções """
-    if opera == '+':
-        soma(n1, n2)
+if opera == '+':
+    soma(n1, n2)
     print('\n')
 
-    elif opera == '-':
-        subtracao(n1, n2)
+elif opera == '-':
+    subtracao(n1, n2)
     print('\n')
 
-    elif opera == '*':
-        multiplicacao(n1, n2)
+elif opera == '*':
+    multiplicacao(n1, n2)
     print('\n')
 
-    elif opera == '/':
-        divisao(n1, n2)
+elif opera == '/':
+    divisao(n1, n2)
     print('\n')
 
-    elif opera == 'r':
-        resto(n1, n2)
+elif opera == 'r':
+    resto(n1, n2)
     print('\n')
 
-    elif opera == 'e':
-        break
+elif opera == 'e':
+    break
 
-    else:
-        print('Erro! Por favor digite uma opção valida na proxima execução')
+else:
+    print('Erro! Por favor digite uma opção valida na proxima execução')
