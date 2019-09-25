e4e71aa102f4572285c0174ddf3c6ed0b0c1b1e7
Eduardo Campacci <campacc@gmail.com>
2019-09-25 15:29:53 -0300

    fazendo commit pq o github ta chato.

--- Fundamentos2/funcao_calc.py
+++ Fundamentos2/funcao_calc.py
@@ -2,23 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """ Funções """
-
-
 def soma(n1, n2):
     print('A soma é.: ', n1 + n2)
-
 
 def subtracao(n1, n2):
     print('A subtração é.: ', n1 - n2)
 
-
 def multiplicacao(n1, n2):
     print('A multiplicação é.: ', n1 * n2)
 
-
 def divisao(n1, n2):
     print('A divisao é.: ', n1 / n2)
-
 
 def resto(n1, n2):
     print('O resto da divisão é.: ', n1 % n2)
@@ -36,17 +30,13 @@ while opera != 'e':
     e p/ encerrar
     \n''')
 
-    # Condição que encerra o programa.
-    if opera == 'e':
-        print('Passando pelo break')
-        break
 
     """ Inputs """
     n1 = int(input('Digite o primeiro valor.: '))
     n2 = int(input('Digite o segundo valor.: '))
 
     """ Chamando as funções """
-    elif opera == '+':
+    if opera == '+':
         soma(n1, n2)
         print('\n')
 
@@ -65,10 +55,13 @@ while opera != 'e':
     elif opera == 'r':
         resto(n1, n2)
         print('\n')
+
     if opera not in 'e':
         print("Calculo Feito")
         break
+
     elif opera == 'e':
         break
+
     else:
         print('Erro! Por favor digite uma opção valida na proxima execução')
