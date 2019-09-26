9b9176e1768548926aabff9f77fd26be5161132e
mariaBet <mariabetania543@gmail.com>
2019-09-26 11:26:40 -0300

    commint minha função que não estou entendo como definir o argumento

---
+++ Fundamentos2/funcaolambda_be.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite um valor: "))
+n3 = int(input("Digite um valor: "))
+valores_expres = lambda n1, n2, n3 : (n1 + n2), ( n2 * n3)
+print('O valor da expressão é: ', valores_expres())
