fb1ac8b213579832da781bd0e7c4521f6d11bd28
Eduardo Campacci <campacc@gmail.com>
2019-09-26 11:36:10 -0300

    \o/

--- Fundamentos2/funcaolambda_be.py
+++ Fundamentos2/funcaolambda_be.py
@@ -4,5 +4,6 @@
 n1 = int(input("Digite um valor: "))
 n2 = int(input("Digite um valor: "))
 n3 = int(input("Digite um valor: "))
-valores_expres = lambda n1, n2, n3 : (n1 + n2), ( n2 * n3)
+#valores_expres = lambda n1, n2, n3 : (n1 + n2), ( n2 * n3)
+valores_expres = lambda: ((n1+n2)-(n2*n3))
 print('O valor da expressão é: ', valores_expres())
