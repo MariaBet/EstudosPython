f971ac129a1c90c712d0d2c8c234cf60a60acbd9
MariaBet <mariabetania543@gmail.com>
2019-10-02 16:03:45 -0300

    update

--- Arquivos_de_Texto/arq_chacal.py
+++ Arquivos_de_Texto/arq_chacal.py
@@ -1,10 +1,2 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
-# Abrindo arquivo
-# (r)-Leitura / (w)-Escrita
-# Senão especificar, o modo (r) sera usado
-arquivo = open('lista_chacal.txt', 'w')
-
-# Lendo o conteudo de um arquivo
-arquivo.read()
