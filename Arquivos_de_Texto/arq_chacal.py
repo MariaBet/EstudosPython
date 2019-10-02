770b17b9ff92d054987cbe03307ce9985babf0a6
Eduardo Campacci <campacc@gmail.com>
2019-10-02 16:36:25 -0300

    update

--- Arquivos_de_Texto/arq_chacal.py
+++ Arquivos_de_Texto/arq_chacal.py
@@ -3,8 +3,22 @@
 
 # Abrindo arquivo
 # (r)-Leitura / (w)-Escrita
+# (x)-Execução - Criar arquivo
 # Senão especificar, o modo (r) sera usado
 arquivo = open('lista_chacal.txt', 'r')
 
+# Escrevendo no arquivo
+arquivo.write(input('Digite o nome do filme ou serie.: '))
+
+
+# Fechando arquivo p/ salvar
+arquivo.close()
+
+
+
+# Abrindo arquivo modo leitura
+arquivo = open('lista_chacal.txt', 'r')
+
 # Lendo o conteudo de um arquivo
+# função read() = EX.: arquivo.read()
 print(arquivo.read())
