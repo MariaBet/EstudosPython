e531602d98612db21a44f2fc40866ee584e478df
MariaBet <mariabetania543@gmail.com>
2019-10-02 16:27:19 -0300

    lista de livros

--- Arquivos_de_Texto/arq_Be.py
+++ Arquivos_de_Texto/arq_Be.py
@@ -4,4 +4,6 @@
 arquivo = open('lista_be.txt', 'w')
 arquivo.write('Apresentação do arquivo' '\n')
 arquivo.write('Lista de livros' '\n')
-arquivo.write('Hercules')
+arquivo.write('Hercules' '\n''O expresso oriente' '\n''Percy Jakson' '\n''Maze Runner''\n'
+              'Jogos Vorazes''\n' 'Mulher Maravilha''\n' 'A maldiação do Tigre''\n'
+              'Guerra do velho''\n''Arena um''\n''Sonhos de avalon''\n''O homem de terno marom')
