2497a5b0ee43b0f34bc254f723f5b16aa0e1647b
MariaBet <mariabetania543@gmail.com>
2019-10-02 16:31:07 -0300

    update lista

--- Arquivos_de_Texto/arq_Be.py
+++ Arquivos_de_Texto/arq_Be.py
@@ -6,4 +6,5 @@ arquivo.write('Apresentação do arquivo' '\n')
 arquivo.write('Lista de livros' '\n')
 arquivo.write('Hercules' '\n''O expresso oriente' '\n''Percy Jakson' '\n''Maze Runner''\n'
               'Jogos Vorazes''\n' 'Mulher Maravilha''\n' 'A maldiação do Tigre''\n'
-              'Guerra do velho''\n''Arena um''\n''Sonhos de avalon''\n''O homem de terno marrom')
+              'Guerra do velho''\n''Arena um''\n''Sonhos de avalon''\n''O homem de terno marrom''\n' 'Endgame''\n'
+              'Mateira Escura''\n''A herdeira da morte''\n' 'Halo''\n' 'O enigma de balckthorn')
