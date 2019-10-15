dbb3215880784db623a98d1b88057eb8c229ce9c
Eduardo Campacci <campacc@gmail.com>
2019-10-15 16:39:15 -0300

    up

--- Arquivos_CSV/arq_csv_Chacal.py
+++ Arquivos_CSV/arq_csv_Chacal.py
@@ -7,6 +7,22 @@ import pandas
 # Carrega dados
 arquivo = pandas.read_csv('dados.csv')
 
-# Imprime informações
+# Imprimindo as 5 primeiras linhas do arquivo
 print(arquivo.head(5))
+
+# Imprimindo as 20 ultimas linhas do arquivo
 print(arquivo.tail(20))
+
+print('\n \n')
+
+# imprimindo uma determinada linha
+print(arquivo.loc[100])
+print('\n \n')
+
+# imprimindo uma determinda linha e coluna
+print(arquivo.loc[10,'Cargo'])
+print('\n \n')
+
+# imprimindo um conjunto em sequencia de linhas e coluna
+print(arquivo.loc[0:19,'Cargo'])
+print('\n \n')
