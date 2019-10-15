d4b3def079386efdb39b40c0f0138e7afe869693
Eduardo Campacci <campacc@gmail.com>
2019-10-15 16:15:21 -0300

    update

--- Arquivos_CSV/arq_csv_Chacal.py
+++ Arquivos_CSV/arq_csv_Chacal.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+# Importar modulo CSV
+import pandas
+
+
+# Carrega dados
+arquivo = pandas.read_csv('dados.csv')
+
+# Imprime informações
+print(arquivo.head(5))
+print(arquivo.tail(20))
