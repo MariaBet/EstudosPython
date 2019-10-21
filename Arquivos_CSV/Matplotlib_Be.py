9cb2226dc6349408bb38cf4418887aaa48109d52
MariaBet <mariabetania543@gmail.com>
2019-10-21 14:31:59 -0300

    matplotlib

--- Arquivos_CSV/Matplotlib_Be.py
+++ Arquivos_CSV/Matplotlib_Be.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5, 6, 7, 8]
-y = [10, 20, 30, 40, 50, 60, 70, 80]
-fig = plt.figure()
-ax = fig.addsubplot(111)
-ax.plot(x, y, color = 'lightblue', linewidth = 3)
-ax.scatter([2, 4, 6], [5, 15, 25], color = 'darkgreen', marker = '^')
-ax.set_xlim(1, 6.5)
-plt.savefig('foo.png')
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
 plt.show()
