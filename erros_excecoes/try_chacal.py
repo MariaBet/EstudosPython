f5b23209012bf7d2d298cd07d0aafcaca1131bbb
MariaBet <mariabetania543@gmail.com>
2019-10-22 14:31:51 -0300

    try e except

--- erros_excecoes/try_chacal.py
+++ erros_excecoes/try_chacal.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
+'''try - bloco permite testar um bloco d codigo quanto a erros
+except bloco permite lidar com o erro
+finally excutar o codigo indepente do resultado do try e Exception'''
+
+
+try:
+    print(n)
+    print("exception")
