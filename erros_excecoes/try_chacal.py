5cb3e78425b077e95730d88651f12e9f19335e8e
Eduardo Campacci <campacc@gmail.com>
2019-10-22 15:14:00 -0300

    Primeiro commit tratamento de erros e exceções.

--- erros_excecoes/try_chacal.py
+++ erros_excecoes/try_chacal.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+x = 12
+y = 0
+
+try:
+    z = x/0
+except ZeroDivisionError:
+    print('Nenhum valor pode ser dividido por zero\n')
+
+
+try:
+    n1 = int(input('Primeiro valor inteiro.: '))
+    n2 = int(input('Segundo valor inteiro.: '))
+except ValueError:
+    print('Por favor! Digite um valor INTEIRO!')
