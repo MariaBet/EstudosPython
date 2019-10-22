dd98f8f88ac143539ea7f973ba349a8a0f888bb9
Eduardo Campacci <campacc@gmail.com>
2019-10-22 15:25:07 -0300

    up

--- erros_excecoes/try_chacal.py
+++ erros_excecoes/try_chacal.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
+
+# Variaveis p/ teste do primeiro erro
 x = 12
 y = 0
 
+# Todo codigo que pode haver erros, deve ser inserido dentro
+# do bloco try
 try:
     z = x/0
+
+# E todo bloco try deve haver uma exceção ou finally para que
+# o erro seja tratado ou ignorado
 except ZeroDivisionError:
     print('Nenhum valor pode ser dividido por zero\n')
 
@@ -15,15 +21,13 @@ try:
     n1 = int(input('Primeiro valor inteiro.: '))
     n2 = int(input('Segundo valor inteiro.: '))
 except ValueError:
-    print('Por favor! Digite um valor INTEIRO!')
-=======
-
-'''try - bloco permite testar um bloco d codigo quanto a erros
-except bloco permite lidar com o erro
-finally excutar o codigo indepente do resultado do try e Exception'''
-
+    print('Por favor! Digite um valor INTEIRO!\n\n')
 
 try:
-    print(n)
-    print("exception")
->>>>>>> f5b23209012bf7d2d298cd07d0aafcaca1131bbb
+    n1 = float(input('Primeiro valor flutuante.: '))
+    n2 = float(input('Segundo valor flutuante.: '))
+except ValueError:
+    print('Por favor! Digite um valor FLUTUANTE, OU SEJA "NAO INTEIRO"'
+    ' com VIRGULA ... ENTENDE USUARIO kkkkk!')
+finally:
+    print('Mesmo havendo erro, o finally sempre sera executado')
